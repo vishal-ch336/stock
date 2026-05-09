f284fd45f6bd5bcdf0ad3edffb12922a8f70fb27
vishal-ch336 <vishalchipurupalli336@gmail.com>
2026-05-09 14:23:25 +0530

    debugging

--- server/src/config.ts
+++ server/src/config.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const configSchema = z.object({
   PORT: z.string().transform(Number).default('8080'),
   MONGO_URI: z.string().default('mongodb://localhost:27017/sungrid?replicaSet=rs0'),
-  CORS_ORIGIN: z.string().default('http://localhost:5173'),
+  CORS_ORIGIN: z.string().transform((val) => val.split(',').map(s => s.trim())).default('http://localhost:5173'),
   REALTIME_WS: z.string().transform(val => val === 'true').default('false'),
   RATE_LIMIT_MAX: z.string().transform(Number).default('180'),
   MAX_UPLOAD_MB: z.string().transform(Number).default('25'),
